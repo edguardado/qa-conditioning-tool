cfe260d959f62045015d12a4cb34c6dc09e43f82
Eduardo Guardado <eduardoguardado@Eduardos-Mac-mini.local>
2023-07-04 09:33:23 -0600

    added create ldap instance endpoint

--- app.js
+++ app.js
@@ -72,6 +72,50 @@ app.get('/setup-ldap-client', async (req, res) => {
 
 })
 
+app.get('/create-ldap-instance', async (req, res) => {
+
+
+  
+  const environment = req.query.env;
+ 
+  if (environment=='stacks' || environment =='stage') {
+   
+    const api = new ApiHelper(environment);
+    const client = new BrowserHelper();
+  
+  
+    let instance = await api.createInstance()
+    console.log(instance.response)
+    if(!instance.includes('AxiosError')){
+      console.log(instance+"/admin")
+      await client.setAdminUser(instance);
+      
+      const url = new URL(instance);
+      const host = url.host;
+
+ 
+
+      await client.login(host);
+      await client.setLdap();
+
+      await client.login(host);
+      await client.addServiceClientLdap();
+
+      res.send(instance+"/admin");
+    }else{
+      res.send("unable to create instance")
+    }
+    
+    
+  } else {
+    res.send('Please provide a valid environment: stacks or stage');
+  }
+
+
+  
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
